f95db21c532331e10f52416f0c57fe60349f439b
LeandroDevLab <prof.oliota@gmail.com>
2025-08-29 20:35:42 -0300

    HOOKS: useState e useEffect sem colaterais

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,82 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
-
+import { useState, useEffect } from 'react';
 import styles from './styles.module.css';
 
+type AvailableThemes = 'dark' | 'light';
+
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+  // evento capturado no onClick
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault();
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+
+    //document.documentElement.setAttribute('data-theme', theme); // efeito colateral, manipulando como um JS normal e não como o React
+  }
+
+  // // FORMA 1
+  // useEffect(() => {
+  //   console.log('useEffect sem dependência', Date.now());
+  // }); // é executado toda vez que o componente renderiza na tela, por qualquer motivo!
+
+  // // FORMA 2 com array de dependência vazia
+  // useEffect(() => {
+  //   console.log('useEffect com array deps vazio', Date.now());
+  // }, []); // Executa apenas quando o React monta o componente na tela pela primeira vez
+  // // Buscar dados de uma API externa e puxa assim que carrega e bastaria buscar uma vez ou para renderização
+
+  // FORMA 3 com dependência citada
+  useEffect(() => {
+    console.log('Theme mudou', theme, Date.now());
+    document.documentElement.setAttribute('data-theme', theme);
+
+    return () => {
+      console.log(
+        'Será executado 1º, antes de todas as mudanças envolvendo esse useEffect',
+      );
+    };
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para a Home'
+        title='Ir para a Home'
+      >
         <HouseIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ver histórico'
+        title='Ver histórico'
+      >
         <HistoryIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Configurações'
+        title='Configurações'
+      >
         <SettingsIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Mudar o tema'
+        title='Mudar o tema'
+        onClick={handleThemeChange}
+      >
         <SunIcon />
       </a>
     </nav>
