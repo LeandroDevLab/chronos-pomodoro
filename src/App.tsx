1718530ce5d723f8d07b4f08aaf9684334ec31b7
LeandroDevLab <prof.oliota@gmail.com>
2025-08-29 21:11:55 -0300

    :recycle: refactor:  criando componente MainForm

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,8 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon } from 'lucide-react';
 import { Footer } from './components/Footer';
+import { MainForm } from './components/MainForm';
 
 export function App() {
   return (
@@ -29,29 +26,7 @@ export function App() {
       </Container>
 
       <Container>
-        <form className='form' action='#'>
-          <div className='formRow'>
-            <DefaultInput
-              id='meuInput'
-              type='text'
-              labelText='task'
-              title='TITULO'
-              placeholder='Digite tarefa'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} color='green' />
-          </div>
-        </form>
+        <MainForm />
       </Container>
 
       <Container>
