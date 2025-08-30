6e9e6ea486bc2ad695d2cbf20ebd436684b9e6d0
LeandroDevLab <prof.oliota@gmail.com>
2025-08-30 00:58:58 -0300

    :construction: createContext, useContext + Provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,28 @@ import './styles/theme.css';
 import './styles/global.css';
 
 import { Home } from './pages/Home';
+import { useState } from 'react';
+import { TaskContext } from './contexts/TaskContext';
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaing: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
-  return <Home />;
+  const [state, setState] = useState(initialState);
+  // o Provider é quem vai prover as informações de value para o componente e seus filhos!
+  return (
+    <TaskContext.Provider value={{ esseQueVaiValer: 321 }}>
+      <Home />
+    </TaskContext.Provider>
+  );
 }
---
+++ src/contexts/TaskContext/index.tsx
@@ -0,0 +1,7 @@
+// Contexto formado de duas coisas
+
+import { createContext } from 'react';
+
+export const TaskContext = createContext({
+  chave: 'valor',
+});
