610580e8f99a79afc877e0ad121d743a06469703
LeandroDevLab <prof.oliota@gmail.com>
2025-08-30 22:07:16 -0300

    :sparkles: feat: Validando e criando nova tarefa

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -5,18 +5,54 @@ import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 import { useRef } from 'react';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import type { TaskModel } from '../../models/TaskModel';
 
 export function MainForm() {
+  const { setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log('Deu Certo!', taskNameInput.current.value);
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    //duration em segundos
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config }, //só para garantir que estou passando tudo de TaskStateModel
+        activeTask: newTask,
+        currentCycle: 1, //conferir depois
+        secondsRemaining, //conferir
+        formattedSecondsRemaining: '00:00', //fazer em função a parte
+        tasks: [...prevState.tasks, newTask], // espalhando o array anterior e adicionando a newTask
+      };
+    });
   }
 
   return (
     <>
-      <form onSubmit={handleCreateNewTask} className={styles.form} action='#'>
+      <form onSubmit={handleCreateNewTask} className={styles.form} action=''>
         <div className={styles.formRow}>
           <DefaultInput
             id='meuInput'
